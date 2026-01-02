f4aa13d1fbc6173f99762f52c6623a80bdf04f9c
Yasin Eren Kovalık <94734316+yasinerenkovalik@users.noreply.github.com>
2026-01-02 22:43:53 +0300

    Remove comments from service registration method
    Removed comments for MediatR, AutoMapper, and FluentValidation from the AddApplicationLayerServices method.

--- TaskManager/Core/TaskManager.Application/ApplicationServiceRegistrations.cs
+++ TaskManager/Core/TaskManager.Application/ApplicationServiceRegistrations.cs
@@ -11,15 +11,10 @@ public static class ServiceRegistrations
     public static IServiceCollection AddApplicationLayerServices(this IServiceCollection services)
     {
         var assembly = Assembly.GetExecutingAssembly();
-
-        // MediatR
         services.AddMediatR(cfg =>
             cfg.RegisterServicesFromAssembly(assembly));
 
-        // AutoMapper
         services.AddAutoMapper(assembly);
-
-        // FluentValidation (tüm validator'lar otomatik)
         services.AddValidatorsFromAssembly(assembly);
 
         return services;
